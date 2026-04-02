47380e898e4b4dd5dfeaa676c36a01d44bee2a75
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2026-04-02 09:16:06 -0600

    Carga de información en l DB

--- 20260331-Laravel/web-laravel/database/migrations/2026_04_01_034919_create_answers_table.php
+++ 20260331-Laravel/web-laravel/database/migrations/2026_04_01_034919_create_answers_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignIn('question_id')->constrained('questions')->onDelete('cascade');
-            $table->foreignIn('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('question_id')->constrained('questions')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
 
             $table->text('content');
             $table->timestamps();
