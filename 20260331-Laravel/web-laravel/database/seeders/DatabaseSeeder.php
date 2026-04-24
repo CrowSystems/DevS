98fe688d9815cf3ab267a22793e5b88277843c77
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2026-04-23 23:12:44 -0600

    configuración de datos en la estructura del programa

--- 20260331-Laravel/web-laravel/database/seeders/DatabaseSeeder.php
+++ 20260331-Laravel/web-laravel/database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'i@test.com',
         ]);
 
         $categories = Category::factory(4)->create();
