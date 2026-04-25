0d0bd45c8b0c5dcbb7c53a5dfeebb42c1ea1873f
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2026-04-24 20:32:48 -0600

    Creación y carga de datos fake para realizar pruebas

--- 20260331-Laravel/web-laravel/database/seeders/DatabaseSeeder.php
+++ 20260331-Laravel/web-laravel/database/seeders/DatabaseSeeder.php
@@ -2,11 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Answer;
 use App\Models\Category;
 use App\Models\Question;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Mockery\Matcher\Any;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,8 +27,25 @@ class DatabaseSeeder extends Seeder
         $categories = Category::factory(4)->create();
 
         $question = Question::factory(30)->create([
-            'category_id' => fn() => $categories->random()->id,
-            'user_id' => fn() => User::inRandomOrder()->first()->id,
+            'category_id'   => fn() => $categories->random()->id,
+            'user_id'       => fn() => User::inRandomOrder()->first()->id,
+        ]);
+
+        $answers = Answer::factory(50)->create([
+            'question_id'   => fn() => $question->random()->id,
+            'user_id'       => fn() => User::inRandomOrder()->first()->id,
+        ]);
+
+        Comment::factory(100)->create([
+            'user_id'               => fn() => User::inRandomOrder()->first()->id,
+            'commentable_id'        => fn() => $answers->random()->id,
+            'comentable_type'       => fn() => Answer::class,
+        ]);
+
+        Comment::factory(100)->create([
+            'user_id'               => fn() => User::inRandomOrder()->first()->id,
+            'commentable_id'        => fn() => $question->random()->id,
+            'comentable_type'       => fn() => Question::class,
         ]);
     }
 }
