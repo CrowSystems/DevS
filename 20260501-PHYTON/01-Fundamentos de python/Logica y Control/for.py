7cfddeed16091b7eb13c206f9ff56dd2988e5d6f
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2026-05-01 09:09:11 -0600

    Manejo de bucle for

---
+++ 20260501-PHYTON/01-Fundamentos de python/Logica y Control/for.py
@@ -0,0 +1,17 @@
+palabra = "Python"
+
+for letra in palabra:
+    print(letra)
+
+    adjetivos = ["Rica", "Saludable"]
+    frutas = ["Manzana", "Naranja", "Kiwi"]
+
+    for adjetivo in adjetivos:
+        for fruta in frutas:
+            print(fruta, adjetivos)
+
+print("-----------------------------------")
+
+for i in range(10):
+    pass
+
