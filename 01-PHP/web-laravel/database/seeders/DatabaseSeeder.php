6a6607b0c25c965f40d782eab88c53ed44236014
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2026-05-04 23:05:09 -0600

    carga de datos en base de datos

--- 01-PHP/web-laravel/database/seeders/DatabaseSeeder.php
+++ 01-PHP/web-laravel/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Question;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'i@etest.com',
+        ]);
+
+        $categories = Category::factory(4)->create();
+
+        $questions = Question::factory(30)->create([
+            'category_id'   =>  fn()    => $categories->random()->id,
+            'user_id'       =>  fn()    => User::inRandomOrder()->first()->id,
         ]);
     }
 }
