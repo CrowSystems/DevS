0565fcd76951be4084f296a59d22701a2fb8d7de
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2026-05-04 20:56:43 -0600

    Carga de modificaciones de estructura de datos

--- 01-PHP/web-laravel/database/migrations/2026_05_05_015937_create_questions_table.php
+++ 01-PHP/web-laravel/database/migrations/2026_05_05_015937_create_questions_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -16,6 +18,9 @@ return new class extends Migration
 
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+
+            $table->string('title');
+            $table->text('description');
 
             $table->timestamps();
         });
