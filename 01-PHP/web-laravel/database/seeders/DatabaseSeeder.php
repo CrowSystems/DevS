f32718b76a7d6181194154763dc533f4a5a1ac2c
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2026-05-10 10:18:12 -0600

    Creación de los factories para el llenado de la base de datos

--- 01-PHP/web-laravel/database/seeders/DatabaseSeeder.php
+++ 01-PHP/web-laravel/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Answer;
 use App\Models\Category;
 use App\Models\Question;
 use App\Models\User;
+use App\Models\Comment;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -25,8 +27,19 @@ class DatabaseSeeder extends Seeder
         $categories = Category::factory(4)->create();
 
         $questions = Question::factory(30)->create([
-            'category_id'   =>  fn()    => $categories->random()->id,
-            'user_id'       =>  fn()    => User::inRandomOrder()->first()->id,
+            'category_id'       =>  fn()    => $categories->random()->id,
+            'user_id'           =>  fn()    => User::inRandomOrder()->first()->id,
+        ]);
+
+        $answers = Answer::factory(50)->create([
+            'question_id'       => fn() => $questions->random()->id,
+            'user_id'           => fn() => User::inRandomOrder()->first()->id,
+        ]);
+
+        Comment::factory(100)->create([
+            'user_id'           => fn() => User::inRandomOrder()->first()->id,
+            'commentable_id'    => fn() => $answers->random()->id,
+
         ]);
     }
 }
